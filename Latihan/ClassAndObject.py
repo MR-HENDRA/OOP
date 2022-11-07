da3896b80477dd5472f5030ea519508e997b001a
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-11-07 23:34:01 +0800

    Class and Object

--- Latihan/ClassAndObject.py
+++ Latihan/ClassAndObject.py
@@ -1,39 +1,42 @@
-class Hero:
+class Hero: #Class
     pass
 
-hero1 = Hero()
+hero1 = Hero() #Object
 hero2 = Hero()
-hero3 = Hero()
-hero4 = Hero();
+hero3 = Hero();
 
 hero1.name = "Naruto"
 hero1.health = 100
+hero1.power = 200
+hero1.armor = 150
 
 hero2.name = "Sazuke"
 hero2.health = 99
+hero2.power = 195
+hero2.armor = 160
 
-hero3.name = "Hinata"
-hero3.health = 95
-
-hero4.name = "Sizuka"
-hero4.health = 90
+hero3.name = "Sizuka"
+hero3.health = 90
+hero3.power = 190
+hero3.armor = 155
 
 print(hero1)
 print(hero1.name)
 print(hero1.health)
+print(hero1.power)
+print(hero1.armor)
 print(hero1.__dict__)
 
 print(hero2)
 print(hero2.name)
 print(hero2.health)
+print(hero2.power)
+print(hero2.armor)
 print(hero2.__dict__)
 
 print(hero3)
 print(hero3.name)
 print(hero3.health)
+print(hero3.power)
+print(hero3.armor)
 print(hero3.__dict__)
-
-print(hero4)
-print(hero4.name)
-print(hero4.health)
-print(hero4.__dict__)
---
+++ Latihan/Constructor.py
@@ -0,0 +1,9 @@
+class Hero:
+    
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
+        
+hero1 = Hero("Naruto")
