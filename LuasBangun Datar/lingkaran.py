7d95e2cbf7f5df871c475441807c76ff3d026415
Hendra Usman <96562868+MR-HENDRA@users.noreply.github.com>
2022-11-26 14:30:14 +0800

    Delete

--- LuasBangun Datar/lingkaran.py
+++
@@ -1,9 +0,0 @@
-def line():
-    print("="*50)
-
-line()
-print(" MENGHITUNG LUAS LINGKARAN ".center(50,"-"))
-jari_jari = int(input("Masukkan Panjang Jari-Jari Lingkaran: "))
-luas_lingkaran = 3.14 * jari_jari ** 2
-print("Luas Lingkaran: ", luas_lingkaran, " cm²")
-line()
